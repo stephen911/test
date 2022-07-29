8f511abe0a0ace8385fa194394305c89918cc604
stephen911 <stephendappah1@gmail.com>
2022-07-29 14:31:15 +0000

    'new'

--- export.py
+++ export.py
@@ -20,4 +20,5 @@ def push():
                         
                             if (path == "" and folderpath == ""):
                                 
-                                
+                                        messagebox.showinfo(title="info", message="No directory selected")
+                                        
