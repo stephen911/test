758644a4cd52684e1473f55638a12a2719b81116
stephen911 <stephendappah1@gmail.com>
2022-07-29 14:18:25 +0000

    'new'

--- fresh.py
+++
@@ -1,8 +0,0 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-
-
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-            <title>NTC CPD Training</title>
-            
