4f9050beffaeb2d59968c18ca7beb3a9b1423881
stephen911 <stephendappah1@gmail.com>
2022-07-29 14:30:41 +0000

    'new'

--- export.py
+++ export.py
@@ -16,4 +16,5 @@ def push():
         path = values["-IN-"]
             folderpath = values["-INI-"]
                 print(path)
-                
+                    print(folderpath)
+                    
