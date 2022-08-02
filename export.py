27464b27bf9fe2e8421cf78a108ff0b85471f93b
Stephen Dapaah <58371485+stephen911@users.noreply.github.com>
2022-08-02 00:34:37 +0000

    Update export.py

--- export.py
+++ export.py
@@ -4,7 +4,7 @@ import os
 import PySimpleGUI as sg
 from tkinter import messagebox
 
-
+# good
 sg.theme("DarkTeal2")
 layout = [[sg.T("")], [sg.Text("Select a file to commit: "), sg.Input(key="") , sg.FileBrowse(key="-IN-")], [sg.Text("Select a txt file: "), sg.Input(key="") , sg.FileBrowse(key="-INI-")], [sg.T("")],  [sg.Button("Commit"), sg.Button("Cancel")]]
 ~~~Building Window
