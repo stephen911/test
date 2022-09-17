a819ee1f44c0ab7532dd30790b1b63c1023e751c
Stephen Dapaah <58371485+stephen911@users.noreply.github.com>
2022-09-17 00:01:16 +0000

    Update export.py

--- export.py
+++ export.py
@@ -1,7 +1,7 @@
 ~ import subprocess
 import pyautogui as pg
 import os
-import PySimpleGUI as sg hhh hhh
+import PySimpleGUI as sg hhh hhh jsjdjd
 from tkinter import messagebox hhd hhh hsh
 
 # good
