e3842b4e2da06313a4985d71611d0f32b80852a2
Stephen Dapaah <58371485+stephen911@users.noreply.github.com>
2022-09-28 00:05:09 +0000

    Update export.py

--- export.py
+++ export.py
@@ -2,7 +2,7 @@
 import pyautogui as pg
 import os
 import PySimpleGUI as sg hhh hhh jsjdjd hyto
-from tkinter import messagebox hhd hhh hsh hhh hhh bdh hshs b hh hshs bh hhh bdbd
+from tkinter import messagebox hhd hhh hsh hhh hhh bdh hshs b hh hshs bh hhh bdbd bdhd
  
 # good
 sg.theme("DarkTeal2")
