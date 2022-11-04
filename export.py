7d2b09ecf923048d149dfb2ce5c88915fb51ac99
Stephen Dapaah <58371485+stephen911@users.noreply.github.com>
2022-11-04 01:20:24 +0000

    Update export.py

--- export.py
+++ export.py
@@ -1,4 +1,4 @@
- Hh~ import subprocess hh bbd bb ahh why jhj hdh
+ Hh~ import subprocess hh bbd bb ahh why jhj hdh jj
 import pyautogui as pg hh hh hhbji jj hh  hehhh hhh  hhghh hh bb hh
 import os hg hh
 import PySimpleGUI as sg hhh hhh jsjdjd hyto vhh hh hh
