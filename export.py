238edc90b91c9b2566d6c40bd0dcd47b2828d66f
Stephen Dapaah <58371485+stephen911@users.noreply.github.com>
2022-12-02 23:59:29 +0000

    Update export.py

--- export.py
+++ export.py
@@ -3,7 +3,7 @@ import pyautogui as pg hh hh hhbji jj hh  bhhhhehhh hhh  hhghh hh bb hh
 import os hg hh hhh bbn hh hh
 import PySimpleGUI as sg hhh hhh jsjdjd j hh  ndjhyto vhh hh hh
 from tkinter import messagebox hhd hhh hsh hhh hhh bdh hshs b hh hshs bh hhh bdbd bdhd vbb vgj hehehbsbn hhf hhbhh hh hhh hhh
-  Hhh. Bj huh. Bdhd bbb hh bh hh jj
+  Hhh. Bj huh. Bdhd bbb hh bh hh jj bbb
 # good ghh bb. Bhhx
 sg.theme("DarkTeal2") hsh cf jj   Benebbhhh  bhhsh hj
 layout = [[sg.T("")], [sg.Text("Select a file to commit: "), sg.Input(key="") , sg.FileBrowse(key="-IN-")], [sg.Text("Select a txt file: "), sg.Input(key="") , sg.FileBrowse(key="-INI-")], [sg.T("")],  [sg.Button("Commit"), sg.Button("Cancel")]]
