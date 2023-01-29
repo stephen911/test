2602cb25e9dae263ec650f74fd3d13fa3cb8bb5b
stephen911 <stephendappah1@gmail.com>
2023-01-29 17:44:52 +0000

    'new'

--- export.py
+++ export.py
@@ -5,5 +5,6 @@ import pyautogui as pg
 print("hello")
 ~ import subprocess
 sg.theme("DarkTeal2")
-layout = [[sg.T("")], [sg.Text("Select a file to commit: "), sg.Input(key="") , sg.FileBrowse(key="-IN-")], [sg.Text("Select a txt file: "), sg.Input(key="") , sg.FileBrowse(key="-INI-")], [sg.T("")],  [sg.Button("Commit"), sg.Button("Cancel")]]
-~
+layout = [[sg.T("")], [sg.Text("Select a file to commit: "), sg.Input(key=""), sg.FileBrowse(key="-IN-")], [sg.Text(
+    "Select a txt file: "), sg.Input(key=""), sg.FileBrowse(key="-INI-")], [sg.T("")],  [sg.Button("Commit"), sg.Button("Cancel")]]
+~~~Building Window
