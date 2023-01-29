eb7e04bfc37792f07390f008a0759adf1542fc2c
stephen911 <stephendappah1@gmail.com>
2023-01-29 10:39:27 +0000

    'new'

--- export.py
+++ export.py
@@ -10,7 +10,8 @@ layout = [[sg.T("")], [sg.Text("Select a file to commit: "), sg.Input(key="") , 
 ~~~Building Window
 window = sg.Window('Stedap Commits', layout, icon="sc.ico", size=(600, 200), element_justification="center")
 
-
+sg.theme("DarkTeal2")
+layout = [[sg.T("")], [sg.Text("Select a file to commit: "), sg.Input(key="") , sg.FileBrowse(key="-IN-")], [sg.Text("Select a txt file: "), sg.Input(key="") , sg.FileBrowse(key="-INI-")], [sg.T("")],  [sg.Button("Commit"), sg.Button("Cancel")]]###Building Window
 
 def push():
         path = values["-IN-"]
