81cb95cea3e3c2b01bbe7511d61812061396558b
stephen911 <stephendappah1@gmail.com>
2023-01-29 11:00:24 +0000

    'new'

--- export.py
+++ export.py
@@ -3,4 +3,6 @@
 ~ pg.press("enter")
 while True:
             event, values = window.read()
-            
+                if event == sg.WIN_CLOSED or event == "Exit":
+                                break
+                        
