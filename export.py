63051bd91b4e91ec7b14e14d569a396f72eff664
stephen911 <stephendappah1@gmail.com>
2023-01-29 10:50:54 +0000

    'new'

--- export.py
+++ export.py
@@ -11,7 +11,9 @@ layout = [[sg.T("")], [sg.Text("Select a file to commit: "), sg.Input(key="") , 
 window = sg.Window('Stedap Commits', layout, icon="sc.ico", size=(600, 200), element_justification="center")
 
 sg.theme("DarkTeal2")
-layout = [[sg.T("")], [sg.Text("Select a file to commit: "), sg.Input(key="") , sg.FileBrowse(key="-IN-")], [sg.Text("Select a txt file: "), sg.Input(key="") , sg.FileBrowse(key="-INI-")], [sg.T("")],  [sg.Button("Commit"), sg.Button("Cancel")]]###Building Window~ import subprocessimport pyautogui as pgimport os
+layout = [[sg.T("")], [sg.Text("Select a file to commit: "), sg.Input(key="") , sg.FileBrowse(key="-IN-")], [sg.Text("Select a txt file: "), sg.Input(key="") , sg.FileBrowse(key="-INI-")], [sg.T("")],  [sg.Button("Commit"), sg.Button("Cancel")]]###Building Window~ import subprocessimport pyautogui as pgimport osimport PySimpleGUI as sg
+from tkinter import messagebox
+
 
 def push():
         path = values["-IN-"]
