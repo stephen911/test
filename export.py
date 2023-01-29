4f7d96273ce386d47b0f63b8da10c11a03d47bb6
stephen911 <stephendappah1@gmail.com>
2023-01-29 10:52:19 +0000

    'new'

--- export.py
+++ export.py
@@ -26,6 +26,9 @@ window = sg.Window('Stedap Commits', layout, icon="sc.ico",
 
 
 def push():
+
+
+def push():
         path = values["-IN-"]
             folderpath = values["-INI-"]
                 print(path)
