ae4769ac6f7bd5a66cf6cc189b851eeccde530ad
stephen911 <stephendappah1@gmail.com>
2023-01-29 17:38:14 +0000

    hh

--- export.py
+++
@@ -1,8 +0,0 @@
-from tkinter import messagebox
-import PySimpleGUI as sg
-import os
-import pyautogui as pg
-~ import subprocess
-sg.theme("DarkTeal2")
-layout = [[sg.T("")], [sg.Text("Select a file to commit: "), sg.Input(key=""), sg.FileBrowse(key="-IN-")], [sg.Text(
-    "Select a txt file: "), sg.Input(key=""), sg.FileBrowse(key="-INI-")], [sg.T("")],  [sg.Button("Commit"), sg.Button("Cancel")]]
