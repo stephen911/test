fcdc0a3d9fd78bd694e7f9dfbf89a4a61be7af7d
stephen911 <stephendappah1@gmail.com>
2023-02-04 13:15:50 +0000

    'new'

--- export.py
+++ export.py
@@ -12,4 +12,5 @@ if (path == "" and folderpath == ""):
                                 
                                         
                                                 
-                                                
+                                            else:
+                                                    
