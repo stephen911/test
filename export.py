939eb569b5ca82ac4f8ee643205875c696b60ab4
stephen911 <stephendappah1@gmail.com>
2023-02-04 13:14:57 +0000

    'new'

--- export.py
+++ export.py
@@ -1,7 +1,6 @@
-    path = values["-IN-"]
-        folderpath = values["-INI-"]
-            print(path)
-                print(folderpath)
-                    
-                        if (path == "" and folderpath == ""):
-                                
+path = values["-IN-"]
+folderpath = values["-INI-"]
+print(path)
+print(folderpath)
+
+if (path == "" and folderpath == ""):
