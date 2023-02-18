14059ee99f99690431ab6719ca7d450c3d9746b8
stephen911 <stephendappah1@gmail.com>
2023-02-18 12:23:44 +0000

    'new'

--- export.py
+++ export.py
@@ -35,4 +35,5 @@ while True:
                                                 
                                                 
                                                         ~ data = subprocess.check_output(["git", "add", "."]).decode("utf-8").split("\n")
-                                                        
+                                                                ~ pg.sleep(3)
+                                                                
