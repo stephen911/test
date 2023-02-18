434a6f6092935649029c621507225dbafcb10904
stephen911 <stephendappah1@gmail.com>
2023-02-18 12:21:26 +0000

    'new'

--- export.py
+++ export.py
@@ -17,4 +17,5 @@ layout = [[sg.T("")], [sg.Text("Select a file to commit: "), sg.Input(key=""), s
                 ~ pg.sleep(3)
                                 ~ pg.write("git commit -m 'Update'")
                                                 ~ pg.press("enter")
-                                                
+                                                                ~ pg.sleep(3)
+                                                                
